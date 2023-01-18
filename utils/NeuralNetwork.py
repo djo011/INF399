7fa3820c9449f039a378e1a9feef8e76f7ae88f0
djo011 <djo011@uib.no>
2023-01-18 13:36:16 +0100

    Add implementation of MLP Classifier

--- utils/NeuralNetwork.py
+++ utils/NeuralNetwork.py
@@ -7,9 +7,6 @@ Created on Tue Nov 15 14:49:23 2022
 
 import torch.nn as nn
 import torch.nn.functional as F
-import os
-os.chdir(r"C:\Users\danie\UiB\Master\Data_gen")
-
 
 class MLP(nn.Module):
     
@@ -27,6 +24,24 @@ class MLP(nn.Module):
         x = self.output(x)
 
         return x
+
+class MLPclassifier(nn.Module):
+
+    def __init__(self, n_species,hidden_layer_size, n_hidden_layer = 2):
+        super(MLPclassifier,self).__init__()
+        self.input = nn.Linear(n_species, hidden_layer_size)
+        self.linear_layers = nn.ModuleList([nn.Linear(hidden_layer_size, hidden_layer_size) 
+                                            for i in range(1,n_hidden_layer)])
+        self.output = nn.Linear(hidden_layer_size, 2)
+    
+    def forward(self, x):
+        x = F.relu(self.input(x))
+        for layer in self.linear_layers:
+            x = F.relu(layer(x))
+        x = F.softmax(self.output(x))
+
+        return x
+    
 
 if __name__ == "__main__":
     net = MLP(8,8,2)
